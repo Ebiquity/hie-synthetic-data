cb7dfb0d1b5961a47b6f028e25b8327c436ad538
jclavin2 <james@feral.pet>
2023-05-20 16:29:29 -0400

    100K

--- generate_ucsf_data.sh
+++ generate_ucsf_data.sh
@@ -1,2 +1,2 @@
-./run_synthea -p 10000  --exporter.baseDirectory="./output_ucsf/" California  "San Francisco" -d "./src/main/resources/modules/"
+./run_synthea -p 100000  --exporter.baseDirectory="./output_ucsf/" California  "San Francisco" -d "./src/main/resources/modules/"
 python3 /knacc2_local/jclavin/hie-synthetic-data/ConvertUCSFEncounters.py
