afb205927f45ea6973f4f164925f36d69ea4296a
jclavin2 <james@feral.pet>
2023-08-06 15:38:20 -0400

    10K claims for Duke

--- generate_duke_claim_transactions.sh
+++ generate_duke_claim_transactions.sh
@@ -1,5 +1,5 @@
 . ./synthetic.config
 cd $SYNTHEA_DIR
-./run_synthea -p 10  --exporter.baseDirectory="./output_duke/" "North Carolina"  "Durham" -d "./src/main/resources/modules/"
+./run_synthea -p 10000  --exporter.baseDirectory="./output_duke/" "North Carolina"  "Durham" -d "./src/main/resources/modules/"
 cd $SYTHEA_RDF_DIR
 python3 ConvertDukeClaimTransactions.py
