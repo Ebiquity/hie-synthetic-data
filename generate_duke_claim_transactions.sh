b5da5da5067746f4b0f2196a05306c3bec61bff0
jclavin2 <james@feral.pet>
2023-09-09 20:11:14 -0400

    Begin move from clm trans to claims

--- generate_duke_claim_transactions.sh
+++
@@ -1,5 +0,0 @@
-. ./synthetic.config
-cd $SYNTHEA_DIR
-./run_synthea -p 10000  --exporter.baseDirectory="./output_duke/" "North Carolina"  "Durham" -d "./src/main/resources/modules/"
-cd $SYTHEA_RDF_DIR
-python3 ConvertDukeClaimTransactions.py
--- generate_duke_claims.sh
+++ generate_duke_claims.sh
@@ -2,11 +2,10 @@
 cd $SYNTHEA_DIR
 ./run_synthea -p 10000  --exporter.baseDirectory="./output_duke/" "North Carolina"  "Durham" -d "./src/main/resources/modules/"
 cd $SYNTHETIC_DATA_GENERATOR_DIR
-python3 ConvertDukeEncounters.py
+python3 ConvertDukeClaims.py
 cd $SYNTHEA_RDF_DIR
 python3 /knacc2_local/jclavin/synthea-rdf/conversion.py
 cd $DEST_PATH
-for x in encounter*.ttl; do
+for x in claim*.ttl; do
     mv -- "$x" "duke_to_wake_${x}"
 done
-
